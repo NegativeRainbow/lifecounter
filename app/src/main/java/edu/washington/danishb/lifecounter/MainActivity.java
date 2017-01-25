14e40b5524c1ff2d58b960eef53ebee6970e20d5
NegativeRainbow <danishb@uw.edu>
2017-01-25 12:03:44 -0800

    Finished setting up layout

--- app/src/main/java/edu/washington/danishb/lifecounter/MainActivity.java
+++ app/src/main/java/edu/washington/danishb/lifecounter/MainActivity.java
@@ -5,6 +5,11 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    int player1 = 20;
+    int player2 = 20;
+    int player3 = 20;
+    int player4 = 20;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
